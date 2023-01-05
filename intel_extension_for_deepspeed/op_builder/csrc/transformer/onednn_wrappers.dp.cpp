3376875a1f1371acb32d52bf1e06d8f34c3a5315
Jin, Youzhi <youzhi.jin@intel.com>
2023-01-05 06:49:55 -0800

    fix onednn upgrade compilation errors

--- intel_extension_for_deepspeed/op_builder/csrc/transformer/onednn_wrappers.dp.cpp
+++ intel_extension_for_deepspeed/op_builder/csrc/transformer/onednn_wrappers.dp.cpp
@@ -67,21 +67,24 @@ inline int onednn_matmul(sycl::queue* handle,
     auto wgt_mem = dnnl::memory(wgt_md, engine, (void*)wgt_ptr);
     auto dst_mem = dnnl::memory(dst_md, engine, (void*)dst_ptr);
 
-    auto matmul_d = dnnl::matmul::desc(src_md, wgt_md, dst_md);
-
     dnnl::primitive_attr attr;
-    if (alpha != 1.0f) attr.set_output_scales(0, {alpha});
+    std::unordered_map<int, dnnl::memory> matmul_args;
+    if (alpha != 1.0f) {
+        float alpha_v(alpha);
+        attr.set_scales_mask(DNNL_ARG_DST, /* mask */ 0);
+        dnnl::memory alpha_mem({{1}, dnnl::memory::data_type::f32, {1}}, engine, &alpha_v);
+        matmul_args.insert({DNNL_ARG_ATTR_SCALES | DNNL_ARG_WEIGHTS, alpha_mem});
+    }
     if (beta != 0.0f) {
         dnnl::post_ops po;
         po.append_sum(beta);
         attr.set_post_ops(po);
     }
 
-    auto matmul_pd = dnnl::matmul::primitive_desc(matmul_d, attr, engine);
+    auto matmul_pd = dnnl::matmul::primitive_desc(engine, src_md, wgt_md, dst_md, attr);
 
     auto matmul_prim = dnnl::matmul(matmul_pd);
 
-    std::unordered_map<int, dnnl::memory> matmul_args;
     matmul_args.insert({DNNL_ARG_SRC, src_mem});
     matmul_args.insert({DNNL_ARG_WEIGHTS, wgt_mem});
     matmul_args.insert({DNNL_ARG_DST, dst_mem});
