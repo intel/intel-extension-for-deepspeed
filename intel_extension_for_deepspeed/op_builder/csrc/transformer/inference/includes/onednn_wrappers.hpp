56f243f00614142c5d6442740859b442a4af1aeb
baodii <di.bao@intel.com>
2023-04-23 20:55:53 -0700

    reorgnize file folders, same as deepspeed

---
+++ intel_extension_for_deepspeed/op_builder/csrc/transformer/inference/includes/onednn_wrappers.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#if __has_include(<sycl/sycl.hpp>)
+#include <sycl/sycl.hpp>
+using namespace sycl;
+#elif __has_include(<CL/sycl.hpp>)
+#include <CL/sycl.hpp>
+using namespace cl::sycl;
+#else
+#error "Unsupported compiler"
+#endif
+#include <ext/oneapi/experimental/bfloat16.hpp>
+#include "compatible.h"
+
+template <typename T>
+int onednn_matmul_ex(sycl::queue handle,
+                     bool trans_src,
+                     bool trans_wgt,
+                     int m,
+                     int n,
+                     int k,
+                     const float alpha,
+                     const float beta,
+                     const T* src_ptr,
+                     const T* wgt_ptr,
+                     T* dst_ptr);
+
+template <typename T>
+int onednn_batchgemm(sycl::queue handle,
+                     int m,
+                     int n,
+                     int k,
+                     const float alpha,
+                     const float beta,
+                     const T* src_ptr,
+                     const T* wgt_ptr,
+                     T* dst_ptr,
+                     bool trans_src,
+                     bool trans_wgt,
+                     int batch);
