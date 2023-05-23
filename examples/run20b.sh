20fc6e3bd75f0ed752136a21c8d45e3d4f3e469d
Yizhou Wang <yizhou.wang@intel.com>
2023-05-23 15:01:57 +0800

    fix typo

--- examples/run20b.sh
+++ examples/run20b.sh
@@ -1,3 +1,4 @@
+echo "!!!please use generate_hostfile.sh to set hostfile for enough nodes before training"
 export WORLD_SIZE=${WORLD_SIZE:-48}
 export MICRO_BATCH=${MICRO_BATCH:-1}
 export NLAYERS=${NLAYERS:-44}
@@ -10,7 +11,6 @@ export TP=${TP:-1}
 export PP=${PP:-1}
 export GLOBAL_BATCH=$(( $WORLD_SIZE * $MICRO_BATCH / $TP / $PP ))
 
-echo "!!!please use generate_hostfile.sh to set hostfile for $((${WORLD_SIZE}/12)) nodes before training"
 export DS_CONFIG=${PWD}/"ds_stage${ZERO_STAGE}_mb${MICRO_BATCH}_gb${GLOBAL_BATCH}_pp${PP}_tp${TP}_bf16.json"
 
 bash $LLM_DK_DIR/intel-extension-for-deepspeed/examples/generate_config.sh
