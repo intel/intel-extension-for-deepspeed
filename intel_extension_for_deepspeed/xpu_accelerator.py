4580424669af59d2910714506dddbd47cb795c3f
Jin, Youzhi <youzhi.jin@intel.com>
2023-09-24 10:20:08 -0700

    fix build error with DeepSpeed triton support

--- intel_extension_for_deepspeed/xpu_accelerator.py
+++ intel_extension_for_deepspeed/xpu_accelerator.py
@@ -143,6 +143,9 @@ class XPU_Accelerator(DeepSpeedAccelerator):
     def communication_backend_name(self):
         return self._communication_backend_name
 
+    def is_triton_supported(self):
+        return False
+
     # Data types
     def is_bf16_supported(self):
         return True
