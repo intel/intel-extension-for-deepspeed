8454a7607b432c8ff8f6be9ce73973bf3f37ca22
Yizhou Wang <yizhou.wang@intel.com>
2024-03-28 03:02:30 +0000

    update

--- intel_extension_for_deepspeed/op_builder/csrc/flash_attn/flash_attn.dp.cpp
+++ intel_extension_for_deepspeed/op_builder/csrc/flash_attn/flash_attn.dp.cpp
@@ -16,16 +16,16 @@ std::vector<torch::Tensor> flash_attn_fwd(torch::Tensor &q,
                                           bool is_causal,
                                           bool is_training,
                                           bool is_dropout) {
-    // torch::Tensor q_ = q.transpose(1, 2).contiguous().transpose(1, 2);
-    // torch::Tensor k_ = k.transpose(1, 2).contiguous().transpose(1, 2);
-    // torch::Tensor v_ = v.transpose(1, 2).contiguous().transpose(1, 2);
-    torch::Tensor output = torch::empty_like(q);
+    torch::Tensor q_ = q.transpose(1, 2).contiguous().transpose(1, 2);
+    torch::Tensor k_ = k.transpose(1, 2).contiguous().transpose(1, 2);
+    torch::Tensor v_ = v.transpose(1, 2).contiguous().transpose(1, 2);
+    torch::Tensor output = torch::empty_like(q_);
     torch::Tensor softmax_L, dropout_mask;
     softmax_L = torch::empty({bs * head_number, 1, seqlens}, q.options()).to(at::kFloat);
 
-    void *q_ptr = (void *)q.data_ptr();
-    void *k_ptr = (void *)k.data_ptr();
-    void *v_ptr = (void *)v.data_ptr();
+    void *q_ptr = (void *)q_.data_ptr();
+    void *k_ptr = (void *)k_.data_ptr();
+    void *v_ptr = (void *)v_.data_ptr();
     void *output_ptr = (void *)output.data_ptr();
     void *softmax_L_ptr = (void *)softmax_L.data_ptr();
     void *drop_mask_ptr = nullptr;
@@ -72,21 +72,21 @@ std::vector<torch::Tensor> flash_attn_bwd(torch::Tensor &gradout,
                                           bool is_causal,
                                           bool is_dropout,
                                           torch::Tensor &softmax_L) {
-    // torch::Tensor q_ = q.transpose(1, 2).contiguous().transpose(1, 2);
-    // torch::Tensor k_ = k.transpose(1, 2).contiguous().transpose(1, 2);
-    // torch::Tensor v_ = v.transpose(1, 2).contiguous().transpose(1, 2);
-    // torch::Tensor out_ = out.transpose(1, 2).contiguous().transpose(1, 2);
-    // torch::Tensor grad_out_ = gradout.transpose(1, 2).contiguous().transpose(1, 2);
+    torch::Tensor q_ = q.transpose(1, 2).contiguous().transpose(1, 2);
+    torch::Tensor k_ = k.transpose(1, 2).contiguous().transpose(1, 2);
+    torch::Tensor v_ = v.transpose(1, 2).contiguous().transpose(1, 2);
+    torch::Tensor out_ = out.transpose(1, 2).contiguous().transpose(1, 2);
+    torch::Tensor grad_out_ = gradout.transpose(1, 2).contiguous().transpose(1, 2);
 
-    torch::Tensor dq = torch::zeros_like(q);
-    torch::Tensor dk = torch::empty_like(k);
-    torch::Tensor dv = torch::empty_like(v);
+    torch::Tensor dq = torch::zeros_like(q_);
+    torch::Tensor dk = torch::empty_like(k_);
+    torch::Tensor dv = torch::empty_like(v_);
     torch::Tensor d_buffer = torch::empty_like(softmax_L);
-    void *gradout_ptr = (void *)gradout.data_ptr();
-    void *q_ptr = (void *)q.data_ptr();
-    void *k_ptr = (void *)k.data_ptr();
-    void *v_ptr = (void *)v.data_ptr();
-    void *out_ptr = (void *)out.data_ptr();
+    void *gradout_ptr = (void *)grad_out_.data_ptr();
+    void *q_ptr = (void *)q_.data_ptr();
+    void *k_ptr = (void *)k_.data_ptr();
+    void *v_ptr = (void *)v_.data_ptr();
+    void *out_ptr = (void *)out_.data_ptr();
     void *dq_ptr = (void *)dq.data_ptr();
     void *dk_ptr = (void *)dk.data_ptr();
     void *dv_ptr = (void *)dv.data_ptr();
--- intel_extension_for_deepspeed/op_builder/flash_attn.py
+++ intel_extension_for_deepspeed/op_builder/flash_attn.py
@@ -4,10 +4,8 @@ Copyright 2020 The Microsoft DeepSpeed Team
 import torch
 import torch.nn as nn
 from torch.autograd import Function
-from .builder import SYCLOpBuilder, sycl_kernel_path, sycl_kernel_include
-
-
-flash_attn_module = None
+from deepspeed.ops.op_builder.builder import OpBuilder, TORCH_MAJOR, TORCH_MINOR
+import fmha_module
 
 
 class FlashAttnFunc(Function):
@@ -28,7 +26,7 @@ class FlashAttnFunc(Function):
         is_training = True
         is_dropout = (dropout_p != 0)
 
-        out, softmax_L = flash_attn_module.flash_attn_fwd(
+        out, softmax_L = fmha_module.flash_attn_fwd(
             query, key, value, bs, hn, sl, hs, softmax_scale,
             dropout_p, dropout_seed,
             is_causal, is_training, is_dropout
@@ -48,7 +46,7 @@ class FlashAttnFunc(Function):
         q, k, v, out, softmax_L = ctx.saved_tensors
         bs, hn, sl, hs = q.shape
 
-        dq, dk, dv = flash_attn_module.flash_attn_bwd(
+        dq, dk, dv = fmha_module.flash_attn_bwd(
             dout, q, k, v, out, bs, hn, sl, hs, ctx.softmax_scale,
             ctx.dropout_p, ctx.dropout_seed,
             ctx.is_causal, ctx.is_dropout, softmax_L
@@ -85,7 +83,7 @@ class FlashAttentionBuilderObject():
 
 
 
-class FlashAttentionBuilder(SYCLOpBuilder):
+class FlashAttentionBuilder(OpBuilder):
     BUILD_VAR = "DS_BUILD_FlashAttention"
     NAME = "flash_attn"
 
@@ -103,28 +101,13 @@ class FlashAttentionBuilder(SYCLOpBuilder):
         ]
 
     def include_paths(self):
-        return [
-            sycl_kernel_include('csrc/includes'),
-            sycl_kernel_include('csrc/flash_attn'),
-            sycl_kernel_include('../../third_party/xetla/include'),
-            'csrc/includes',
-            'csrc/flash_attn',
-            '../../third_party/xetla/include',
-        ]
+        return []
 
     def extra_ldflags(self):
-        args = ['-fsycl', '-fPIC', '-Wl,-export-dynamic']
-        args += ['-fsycl-targets=spir64_gen']
-        args += ["-Xs \"-device pvc -options '-vc-disable-indvars-opt -vc-codegen -doubleGRF -Xfinalizer -printregusage -Xfinalizer -enableBCR -DPASTokenReduction '\" "]
-        return args
+        return []
 
     def cxx_args(self):
-        args = ['-fsycl', '-O3', '-std=c++20', '-w', '-fPIC', '-DMKL_ILP64']
-        args += ['-fsycl-targets=spir64_gen']
-        args += ["-Xs \"-device pvc -options '-vc-disable-indvars-opt -vc-codegen -doubleGRF -Xfinalizer -printregusage -Xfinalizer -enableBCR -DPASTokenReduction '\" "]
-        return args
+        return []
 
     def load(self):
-        global flash_attn_module
-        flash_attn_module = super().load()
         return FlashAttentionBuilderObject()
